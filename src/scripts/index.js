48d673a22517b3ddcb7635eec07474614bc32d77
EvaAramburu <you@example.com>
2023-01-05 11:33:20 +0100

    first test try

---
+++ src/scripts/index.js
@@ -0,0 +1,54 @@
+const table = document.getElementById('table');
+
+// Submit button
+let submit = document.getElementById('submit');
+submit.addEventListener('click', addInput);
+
+// Input addition
+function addInput(){
+
+    const names = document.getElementById('nInput').value;
+    const lastNames = document.getElementById('lInput').value;
+    const birthyear = document.getElementById('bYear').value;
+    const instructor = document.getElementById('inst').value;
+
+    let row=document.createElement("tr");
+ 
+    let fName=document.createElement("td");
+    let lName=document.createElement("td");
+    let bYear=document.createElement("td");
+    let inst= document.createElement("td");
+    let dlt= document.createElement("td");
+
+    fName.innerHTML=names;
+    lName.innerHTML=lastNames;
+    bYear.innerHTML=birthyear;
+    inst.innerHTML=instructor;
+    
+    let dltButton = document.createElement('div');
+    dltButton.className= 'dltIcon';
+
+    row.appendChild(fName);
+    row.appendChild(lName);
+    row.appendChild(bYear);
+    row.appendChild(inst);
+    row.appendChild(dlt);
+    dlt.appendChild(dltButton);
+
+    table.appendChild(row);
+
+    document.getElementById('nInput').value="";
+    document.getElementById('lInput').value="";
+    document.getElementById('bYear').value="";
+    document.getElementById('inst').value="";
+    document.getElementById('nInput').focus();
+
+    // Delete button 
+
+    dltButton.addEventListener("click", erase);
+
+    function erase(){
+            dltButton.closest('tr').remove();
+    }
+
+}
